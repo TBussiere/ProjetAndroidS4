118b55ed2a27c4f80d1df03cc02b7e3ac1c06c80
BUSSIERE THIBAULT p1601465 <thibault.bussiere@etu.univ-lyon1.fr>
2018-04-05 15:51:28 +0200

    icone plus jolie

--- app/src/main/java/com/example/lewho/projectappmobile/FullscreenActivity.java
+++ app/src/main/java/com/example/lewho/projectappmobile/FullscreenActivity.java
@@ -101,7 +101,7 @@ public class FullscreenActivity extends AppCompatActivity {
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        mContentView = findViewById(R.id.imageView2);
 
 
         // Set up the user interaction to manually show or hide the system UI.
